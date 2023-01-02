d96204e34024c83c9d8cfb5ca0b601becd69d036
yunkepeng <yunke.peng@usys.ethz.ch>
2023-01-02 18:52:54 +0100

    first round experiment results

--- co2_combination.R
+++ co2_combination.R
@@ -4,6 +4,9 @@ library(dplyr)
 library(lme4)
 library(MuMIn)
 library(lmerTest)
+source("/Users/yunpeng/yunkepeng/CNuptake_MS/R/stepwise.R")
+source("/Users/yunpeng/yunkepeng/CNuptake_MS/R/stepwise_lm.R")
+library(visreg)
 #include gcme database
 kevin_othervars <- read.csv("~/data/gcme/kevin_20220222/MESI_2022.csv")
 kevin_othervars <- rename(kevin_othervars, c(ambient = x_c, elevated=x_t, ambient_Sd=sd_c, elevated_Sd=sd_t,ambient_Se=se_c,elevated_Se=se_t,n_plots=rep_c,
@@ -27,11 +30,12 @@ unique(kevin_othervars_n2o_w[,c("lon","lat","citation")])
 #All already repeated
 
 #co2-only effect
-df1 <- read.csv("~/data/n2o_wang_oikos/n2o_tables1.csv")
+df1 <- read_csv("~/data/n2o_wang_oikos/n2o_tables1.csv")
 summary(df1)
 names(df1) <- c("ref","location","n_amb","n_elv","n_site","n2o_amb",
                 "n2o_elv","co2_amb","co2_elv","dco2","duration","pft","logr",
-                "weight","group","method","species","Nfer","other")
+                "weight","group","method","species","Nfer","other","latitude","longitude","lat","lon","comments")
+
 df1$Nfer <- as.numeric(df1$Nfer)
 df1$co2_amb <- as.numeric(df1$co2_amb)
 df1$co2_elv <- as.numeric(df1$co2_elv)
@@ -53,81 +57,68 @@ df1$logr[df1$logr=="-Inf"] <- NA
 df1$log_dn2o[df1$log_dn2o=="-Inf"] <- NA
 df1$log_dn2o[df1$log_dn2o=="-Inf"] <- NA
 
+#now, start coordinates conversion
+#add fapar3g from 1/12 resolution (monthly max and mean)
+fapar3g_df_zhu <- read.csv("~/data/n2o_Yunke/forcing/co2_siteinfo_measurementyear_fapar3g_zhu.csv")
 
-ggplot(df1)+geom_point(aes(x=dco2,y=logr,color=nferinfo))+geom_smooth(aes(x=dco2,y=logr),method="lm")
-ggplot(df1)+geom_point(aes(x=dco2,y=logr,color=pft))+geom_smooth(aes(x=dco2,y=logr),method="lm",color="black")
-ggplot(df1)+geom_point(aes(x=dco2,y=logr,color=other_exp))+geom_smooth(aes(x=dco2,y=logr),method="lm",color="black")
+summary(fapar3g_df_zhu)
 
-#ggplot(df1)+geom_point(aes(x=dco2,y=logr,color=pft))+geom_smooth(aes(x=dco2,y=logr,color=pft),method="lm")
-#looks ok
-summary(lm(logr~dco2,df1))
+fapar3g_df_zhu <- fapar3g_df_zhu %>% mutate(min_fapar = coalesce(min_fapar_nfocal0,min_fapar_nfocal1,min_fapar_nfocal2)) %>%
+  mutate(mean_fapar = coalesce(mean_fapar_nfocal0,mean_fapar_nfocal1,mean_fapar_nfocal2)) %>%
+  mutate(max_fapar = coalesce(max_fapar_nfocal0,max_fapar_nfocal1,max_fapar_nfocal2))
+fapar3g_df_zhu <- fapar3g_df_zhu[,c("lon","lat","min_fapar","mean_fapar","max_fapar")]
+fapar3g_df_zhu2 <- aggregate(fapar3g_df_zhu,by=list(fapar3g_df_zhu$lon,fapar3g_df_zhu$lat), mean,na.rm=TRUE)
 
-#combine into site-mean (or basing on citations, that is, site-mean and measurement year)?
-unique(df1$ref)
-df1_plotmean <- aggregate(df1,by=list(df1$ref,df1$nferinfo,df1$pft), FUN=mean, na.rm=TRUE)
-dim(df1_plotmean)
+#read climate and soil predictors
+climates_soil <- read.csv("~/data/n2o_Yunke/forcing/co2_siteinfo_predictors.csv")
+climate_soil2 <- aggregate(climates_soil,by=list(climates_soil$lon,climates_soil$lat), mean,na.rm=TRUE)
 
-ggplot(df1_plotmean)+geom_point(aes(x=dco2,y=logr,color=Group.2,shape=Group.3),size=3)+geom_smooth(aes(x=dco2,y=logr),method="lm")+ylab("log (N2O-e / N2O-a)")
-summary(lm(logr~ dco2,df1_plotmean)) # R2 = 0.24
+#merge with both
+df1_a <- merge(df1,fapar3g_df_zhu2,by=c("lon","lat"),all.x=TRUE)
 
-df1_plotmean$dco2_2 <- df1_plotmean$dco2^2
-ggplot(df1_plotmean)+geom_point(aes(x=dco2,y=logr,color=Group.2,shape=Group.3),size=3)+geom_smooth(aes(x=dco2,y=logr),method="lm",formula=y~0+x)+ylab("log (N2O-e / N2O-a)")
-model1 <- (lm(logr~ -1+dco2+dco2_2,df1_plotmean)) # R2 = 0.24
-co2Values <- seq(0, 400, 0.1)
+df1_all <- merge(df1_a,climate_soil2[,c("lon","lat","PPFD_total_fapar","PPFD_total","Tg","PPFD","vpd","alpha","ndep","ORGC")],
+                 by=c("lon","lat"),all.x=TRUE)
 
+#create site-name
+sitename <- unique(df1_all[,c("lon","lat")])
+sitename$site_a <- paste0("site",c(1:nrow(sitename)))
+sitename <- na.omit(sitename)
+  
+df1_all <- merge(df1_all,sitename,
+                 by=c("lon","lat"),all.x=TRUE)
 
-model_predict <- predict(model1,list(dco2=co2Values, 
-                                        dco2_2=co2Values^2))
-plot(df1_plotmean$dco2, df1_plotmean$logr,pch=16)
-lines(co2Values, model_predict, col='blue')
-df1_line <- as.data.frame(cbind(co2Values,model_predict))
-ggplot(df1_plotmean)+geom_point(aes(x=dco2,y=logr,color=Group.2,shape=Group.3),size=3)+
-  geom_line(data=df1_line,aes(x=co2Values,y=model_predict))+ylab("log (N2O-e / N2O-a)")
+df1_all$PPFD_a <- log(df1_all$PPFD)
+df1_all$PPFD_total_a <- log(df1_all$PPFD_total)
+df1_all$vpd_a <- log(df1_all$vpd)
+df1_all$ndep_a <- log(df1_all$ndep)
+df1_all$orgc_a <- log(df1_all$ORGC)
 
+df1_all[sapply(df1_all, is.infinite)] <- NA
+df1_all$Nfer_a <- sqrt(df1_all$Nfer)
+df1_all$Nfer_a[is.na(df1_all$Nfer_a)==T] <- 0
 
-#subset(df1,nferinfo=="no")
-df1 %>%
-  ggplot() +
-  geom_boxplot( aes(x=pft, y=sen_n2o),alpha = 0.6,outlier.shape = NA)+
-  geom_boxplot(data=df1,aes(y=sen_n2o),alpha = 0.6,outlier.shape = NA)+
-  geom_jitter(aes(x=pft, y=sen_n2o,color=nferinfo),size=2,width = 0.3) +
-  geom_hline( linetype = 'dotted',yintercept=0.0, size=0.5)+ 
-  theme_classic()+coord_flip()+theme(axis.text=element_text(size=12))+
-  labs(x="", y=~paste(N2O," response to ",eCO[2])) 
+df1_all_test <- na.omit(df1_all[,c("log_co2","Nfer_a","min_fapar","mean_fapar","max_fapar","PPFD_total_a","PPFD_a",
+                                   "Tg","vpd_a","ndep_a","orgc_a","logr")])
+stepwise_lm(df1_all_test,"logr")[[1]]
+stepwise_lm(df1_all_test,"logr")[[2]]
 
-df1 %>%
-  ggplot( aes(x=pft, y=logr)) +
-  geom_boxplot(alpha = 0.6,outlier.shape = NA)+
-  geom_jitter(aes(size=1/weight)) + #weight = na * ni / (na+ni))
-  geom_hline( linetype = 'dotted',yintercept=0.0, size=0.5)+ 
-  theme_classic()+coord_flip()+theme(axis.text=element_text(size=12))+
-  labs(x="", y=~paste(N2O," response to ",eCO[2])) 
+mod1 <- (lm(logr~log_co2+Nfer_a+PPFD_a+vpd_a+mean_fapar,df1_all_test))
+summary(mod1)
+length(unique(df1_all_test$Tg))
+mod1a <- visreg(mod1,"log_co2",type="contrast")
+mod1b <-visreg(mod1,"Nfer_a",type="contrast")
+mod1c <- visreg(mod1,"PPFD_a",type="contrast")
+mod1d <- visreg(mod1,"vpd_a",type="contrast")
+mod1e <- visreg(mod1,"mean_fapar",type="contrast")
 
-dim(subset(df1,pft=="Grassland"))
-
-#defined by groups
-subset(df1,group=="A")$dco2
-subset(df1,group=="B")$dco2
-subset(df1,group=="C")$dco2
-df1$group[df1$group=="A"] <- "0-150ppm"
-df1$group[df1$group=="B"] <- "150-300ppm"
-df1$group[df1$group=="C"] <- ">300ppm"
-
-df1 %>%
-  ggplot() +
-  geom_boxplot( aes(x=group, y=sen_n2o),alpha = 0.6,outlier.shape = NA)+
-  geom_boxplot(data=df1,aes(y=sen_n2o),alpha = 0.6,outlier.shape = NA)+
-  geom_jitter(aes(x=group, y=sen_n2o,shape=pft,color=nferinfo),size=2,width = 0.3) +
-  geom_hline( linetype = 'dotted',yintercept=0.0, size=0.5)+ 
-  theme_classic()+coord_flip()+theme(axis.text=element_text(size=12))+
-  labs(x="", y=~paste(N2O," response to ",eCO[2])) 
+summary(lmer(logr~log_co2+Nfer_a+PPFD_a+vpd_a+mean_fapar,df1_all))
 
 #warming only effect
-df2 <- read.csv("~/data/n2o_wang_oikos/n2o_tables2.csv")
+df2 <- read_csv("~/data/n2o_wang_oikos/n2o_tables2.csv")
 summary(df2)
 names(df2) <- c("ref","location","n_amb","n_elv","n_site","n2o_amb",
                 "n2o_elv","dT","duration","pft","logr",
-                "weight","group","species","Nfer","other")
+                "weight","group","species","Nfer","other","latitude","longitude","lat","lon","comments")
 
 df2$Nfer <- as.numeric(df2$Nfer)
 
@@ -142,14 +133,59 @@ df2$sen_n2o <- df2$logr/df2$dT
 df2$nferinfo<- "no"
 df2$nferinfo[df2$Nfer>0]<- "fertilized"
 
-ggplot(df2,aes(x=dT,y=logr))+geom_point(aes(shape=pft,color=nferinfo))+
-  geom_smooth(method="lm")
-summary(lm(logr~dT,df2))
-
 #QQQ: the one with very low logr looks confusing, (ref = Zhao et al., 2017), n2o decreased from 61440 to 1680??? While another sample of this ref looks ok. 
 #not found in paper!!! - In Fig.6 it seems that at AS species was decreased by -15%.
 #shall we remove them? 
 df2 <- subset(df2,logr!=min(df2$logr,na.rm=T))
+
+#merge with both
+df2_a <- merge(df2,fapar3g_df_zhu2,by=c("lon","lat"),all.x=TRUE)
+
+df2_all <- merge(df2_a,climate_soil2[,c("lon","lat","PPFD_total_fapar","PPFD_total","Tg","PPFD","vpd","alpha","ndep","ORGC")],
+                 by=c("lon","lat"),all.x=TRUE)
+
+#create site-name
+sitename <- unique(df2_all[,c("lon","lat")])
+sitename$site_a <- paste0("site",c(1:nrow(sitename)))
+sitename <- na.omit(sitename)
+
+df2_all <- merge(df2_all,sitename,
+                 by=c("lon","lat"),all.x=TRUE)
+
+df2_all$PPFD_a <- log(df2_all$PPFD)
+df2_all$PPFD_total_a <- log(df2_all$PPFD_total)
+df2_all$vpd_a <- log(df2_all$vpd)
+df2_all$ndep_a <- log(df2_all$ndep)
+df2_all$orgc_a <- log(df2_all$ORGC)
+
+df2_all[sapply(df2_all, is.infinite)] <- NA
+df2_all$Nfer_a <- sqrt(df2_all$Nfer)
+df2_all$Nfer_a[is.na(df2_all$Nfer_a)==T] <- 0
+
+df2_all_test <- na.omit(df2_all[,c("dT","Nfer_a","min_fapar","mean_fapar","max_fapar","PPFD_a",
+                                   "vpd_a","ndep_a","orgc_a","logr")])
+
+length(unique(df2_all_test$vpd_a))
+
+mod1a <- visreg(mod1,"log_co2",type="contrast")
+mod1b <-visreg(mod1,"Nfer_a",type="contrast")
+
+stepwise_lm(df2_all_test,"logr")[[1]]
+stepwise_lm(df2_all_test,"logr")[[2]]
+
+mod2 <- (lm(logr~orgc_a+dT,df2_all_test))
+summary(mod2)
+mod2a <- visreg(mod2,"orgc_a",type="contrast")
+mod2b <-visreg(mod2,"dT",type="contrast")
+
+#not used
+df2_all_test <- na.omit(df2_all[,c("dT","Nfer_a","min_fapar","mean_fapar","max_fapar","PPFD_total_a",
+                                   "vpd_a","ndep_a","orgc_a","logr")])
+stepwise_lm(df2_all_test,"logr")[[1]]
+stepwise_lm(df2_all_test,"logr")[[2]]
+
+summary(lm(logr~orgc_a+dT,df2_all_test))
+
 
 df2_plotmean <- aggregate(df2,by=list(df2$ref,df2$nferinfo,df2$pft), FUN=mean, na.rm=TRUE)
 dim(df1_plotmean)
@@ -202,7 +238,7 @@ df2 %>%
   labs(x="", y=~paste(N2O," response to ",eCO[2])) 
 
 #co2 and warming combination effects
-df3 <- read.csv("~/data/n2o_wang_oikos/n2o_tables3.csv")
+df3 <- read_csv("~/data/n2o_wang_oikos/n2o_tables3.csv")
 summary(df3)
 names(df3) <- c("ref","location","n_amb","n_elv","n_site","n2o_amb",
                 "n2o_elv","dT","co2_amb","co2_elv","dCO2","duration","pft","logr",
